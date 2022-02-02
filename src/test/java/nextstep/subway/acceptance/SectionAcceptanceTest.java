114e1a4878f5abbcf6e8c8acc9e20816a3cf7198
sombrero104 <sombrero.khy@gmail.com>
2022-02-02 12:47:09 +0900

    test: 추가된 기능에 대한 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/SectionAcceptanceTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
-import static nextstep.subway.acceptance.LineSteps.지하철_노선_생성_요청;
+import static nextstep.subway.acceptance.LineSteps.*;
 import static nextstep.subway.acceptance.SectionSteps.*;
-import static nextstep.subway.acceptance.StationSteps.지하철역_생성_요청;
+import static nextstep.subway.acceptance.StationSteps.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("구간 관리 기능")
@@ -21,13 +21,14 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     private static final String 강남역 = "강남역";
     private static final String 양재역 = "양재역";
     private static final String 양재시민의숲역 = "양재시민의숲역";
+    private static final String 미금역 = "미금역";
     private static final String 동천역 = "동천역";
-    private static final String 수지구청역 = "수지구청역";
-    private static final int 강남역_양재역_거리 = 1;
-    private static final int 양재역_양재시민의숲역_거리 = 2;
-    private static final int 동천역_수지구청역_거리 = 3;
 
-    private Long 강남역_아이디, 양재역_아이디, 양재시민의숲역_아이디, 동천역_아이디, 수지구청역_아이디, 신분당선_아이디;
+    private static final int 강남역_양재시민의숲역_거리 = 7;
+    private static final int 강남역_양재역_거리 = 4;
+    private static final int 미금역_동천역_거리 = 3;
+
+    private Long 강남역_아이디, 양재역_아이디, 양재시민의숲역_아이디, 미금역_아이디, 동천역_아이디, 신분당선_아이디;
     private String 신분당선_구간_URI;
 
     @BeforeEach
@@ -35,10 +36,10 @@ public class SectionAcceptanceTest extends AcceptanceTest {
         강남역_아이디 = 지하철역_생성_요청(강남역).jsonPath().getLong("id");
         양재역_아이디 = 지하철역_생성_요청(양재역).jsonPath().getLong("id");
         양재시민의숲역_아이디 = 지하철역_생성_요청(양재시민의숲역).jsonPath().getLong("id");
+        미금역_아이디 = 지하철역_생성_요청(미금역).jsonPath().getLong("id");
         동천역_아이디 = 지하철역_생성_요청(동천역).jsonPath().getLong("id");
-        수지구청역_아이디 = 지하철역_생성_요청(수지구청역).jsonPath().getLong("id");
 
-        ExtractableResponse<Response> 지하철_노선_생성_요청 = 지하철_노선_생성_요청(신분당선, BG_RED_600, 강남역_아이디, 양재역_아이디, 강남역_양재역_거리);
+        ExtractableResponse<Response> 지하철_노선_생성_요청 = 지하철_노선_생성_요청(신분당선, BG_RED_600, 강남역_아이디, 양재시민의숲역_아이디, 강남역_양재시민의숲역_거리);
         신분당선_아이디 = 지하철_노선_생성_요청.jsonPath().getLong("id");
         신분당선_구간_URI = 지하철_노선_생성_요청.header("Location") + "/sections";
     }
@@ -51,11 +52,54 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     @Test
     void createSection() {
         // when
-        ExtractableResponse<Response> createResponse = 구간_생성_요청(신분당선_아이디, 양재역_아이디, 양재시민의숲역_아이디, 양재역_양재시민의숲역_거리);
+        구간_생성_요청(신분당선_아이디, 강남역_아이디, 양재역_아이디, 강남역_양재역_거리);
 
         // then
-        assertThat(createResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(createResponse.header("Location")).isNotBlank();
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역_아이디, 양재역_아이디, 양재시민의숲역_아이디);
+    }
+
+    /**
+     * When 역 사이에 새로운 역을 등록할 경우 기존 역 사이 길이보다 크거나 같으면
+     * Then 새로운 구간 등록이 실패한다
+     */
+    @DisplayName("기존 역 사이 길이보다 크거나 같은 구간을 등록")
+    @Test
+    void addLineSectionOverLength() {
+        // when
+        ExtractableResponse<Response> createRequest = 구간_생성_요청(신분당선_아이디, 강남역_아이디, 양재역_아이디, 강남역_양재시민의숲역_거리);
+
+        // then
+        assertThat(createRequest.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * When 상행역과 하행역이 이미 노선에 모두 등록되어 있다면
+     * Then 새로운 구간 등록이 실패한다
+     */
+    @DisplayName("상행역과 하행역이 이미 노선에 모두 등록되어 있는 경우 구간을 등록")
+    @Test
+    void addDuplicateLineSection() {
+        // when
+        ExtractableResponse<Response> createRequest = 구간_생성_요청(신분당선_아이디, 강남역_아이디, 양재시민의숲역_아이디, 강남역_양재시민의숲역_거리);
+
+        // then
+        assertThat(createRequest.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    /**
+     * When 상행역과 하행역 둘 중 하나도 포함되어 있지 않으면
+     * Then 새로운 구간 등록이 실패한다
+     */
+    @DisplayName("상행역과 하행역 둘 중 하나도 포함되어 있지 않은 경우 구간을 등록")
+    @Test
+    void addNonConnectableLineSection() {
+        // when
+        ExtractableResponse<Response> createRequest = 구간_생성_요청(신분당선_아이디, 미금역_아이디, 동천역_아이디, 미금역_동천역_거리);
+
+        // then
+        assertThat(createRequest.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     /**
@@ -66,13 +110,15 @@ public class SectionAcceptanceTest extends AcceptanceTest {
     @Test
     void deleteSection() {
         // given
-        ExtractableResponse<Response> createResponse = 구간_생성_요청(신분당선_아이디, 양재역_아이디, 양재시민의숲역_아이디, 양재역_양재시민의숲역_거리);
+        구간_생성_요청(신분당선_아이디, 강남역_아이디, 양재역_아이디, 강남역_양재역_거리);
 
         // when
-        ExtractableResponse<Response> deleteResponse = 구간_제거_요청(createResponse.header("Location"), 양재시민의숲역_아이디);
+        구간_제거_요청(신분당선_구간_URI, 양재역_아이디);
 
         // then
-        assertThat(deleteResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        ExtractableResponse<Response> response = 지하철_노선_조회_요청(신분당선);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("stations.id", Long.class)).containsExactly(강남역_아이디, 양재시민의숲역_아이디);
     }
 
 }
